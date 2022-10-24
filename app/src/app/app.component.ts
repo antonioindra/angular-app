2284bad677ec1f8b35df22e63a8b5f28c9b8020d
agenadiev <agenadiev@minsait.com>
2022-10-24 11:24:15 +0200

    componentes

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { getLocaleExtraDayPeriodRules } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  edad:number=1;
 }
+
+/*
+getEdad() {
+  return this.edad;
+}*/
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,12 +6,26 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './component/users/users.component';
 import { LoginComponent } from './component/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { TopbarComponent } from './component/topbar/topbar.component';
+import { SaldoComponent } from './component/saldo/saldo.component';
+import { DatosEquipoComponent } from './component/datos-equipo/datos-equipo.component';
+import { DatosClienteComponent } from './component/datos-cliente/datos-cliente.component';
+import { RecomendacionesComponent } from './component/recomendaciones/recomendaciones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    NavbarComponent,
+    TopbarComponent,
+    SaldoComponent,
+    DatosEquipoComponent,
+    DatosClienteComponent,
+    RecomendacionesComponent
   ],
   imports: [
     BrowserModule,
