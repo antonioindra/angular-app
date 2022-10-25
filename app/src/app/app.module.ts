7c137f3d1d431e7009be10e11a6af73271b581c6
agenadiev <agenadiev@minsait.com>
2022-10-25 10:18:17 +0200

    formularios

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './component/users/users.component';
 import { LoginComponent } from './component/login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { TopbarComponent } from './component/topbar/topbar.component';
@@ -13,6 +13,19 @@ import { SaldoComponent } from './component/saldo/saldo.component';
 import { DatosEquipoComponent } from './component/datos-equipo/datos-equipo.component';
 import { DatosClienteComponent } from './component/datos-cliente/datos-cliente.component';
 import { RecomendacionesComponent } from './component/recomendaciones/recomendaciones.component';
+import { TemplateDrivenComponent } from './formularios/template-driven/template-driven.component';
+import { ReactiveFormComponent } from './formularios/reactive-form/reactive-form.component';
+
+/*material */
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import { BasicFormComponent } from './formularios/basic-form/basic-form.component';
+import { NestedFormComponent } from './formularios/nested-form/nested-form.component';
+import { ArrayFormComponent } from './formularios/array-form/array-form.component';
 
 @NgModule({
   declarations: [
@@ -25,12 +38,27 @@ import { RecomendacionesComponent } from './component/recomendaciones/recomendac
     SaldoComponent,
     DatosEquipoComponent,
     DatosClienteComponent,
-    RecomendacionesComponent
+    RecomendacionesComponent,
+    TemplateDrivenComponent,
+    ReactiveFormComponent,
+    BasicFormComponent,
+    NestedFormComponent,
+    ArrayFormComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    /*meterial*/
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
